3e6fe7b3b68a4661d20c423fc190b93d2d7336fc
Fernanda Freitas Martinez <idea.fernanda@gmail.com>
2022-07-04 13:03:49 +0100

    refactor form_for

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
+  get '/', to: 'tasks#index'
+
   # Create
   get 'task/new', to: 'tasks#new'
-
   post 'tasks', to: 'tasks#create'
 
-  # Read (all)
+  # Read (all and one)
   get 'tasks', to: 'tasks#index', as: :tasks_all
-
-  # Read (one)
   get 'tasks/:id', to: 'tasks#show', as: :task
 
   # Update
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
-
   patch 'tasks/:id', to: 'tasks#update'
 
   # Delete
